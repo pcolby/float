4ad65d87abc99d9ef7e82c542d6dd0cc159cc6df
Paul Colby <git@colby.id.au>
2019-10-16 23:10:27 +1100

    Add copyright / GPLv3 license notices

--- src/fitbit.h
+++ src/fitbit.h
@@ -1,3 +1,22 @@
+/*
+    Copyright 2019 Paul Colby <git@colby.id.au>
+
+    This file is part of Float.
+
+    Float is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Float is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Float.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <QObject>
 #include <QUrl>
 #include <QWebEnginePage>
